563a1319bad1922d554f1b5868e40fbb93cf71cf
valkryhx <283306449@qq.com>
2023-07-07 01:02:28 +0800

    Update new_train_qlora_chatglm2.py

--- new_train_qlora_chatglm2.py
+++ new_train_qlora_chatglm2.py
@@ -208,7 +208,7 @@ def train(global_args):
     },
     "train_batch_size": 2,
 }
-
+    from transformers.deepspeed import HfDeepSpeedConfig
     dschf = HfDeepSpeedConfig(ds_config) 
     #ds_config_file = global_args.deepspeed 
     #dschf = HfDeepSpeedConfig(ds_config_file)
