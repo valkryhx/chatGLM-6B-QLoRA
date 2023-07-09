ce894b9be17fb557044ad0643ca0ad5022ab9a5f
valkryhx <283306449@qq.com>
2023-07-09 14:34:47 +0800

    Update train_qlora_deepspeed_zero.py

--- train_qlora_deepspeed_zero.py
+++ train_qlora_deepspeed_zero.py
@@ -24,7 +24,7 @@ from transformers import (
 )
 from peft import (
     TaskType,
-    #LoraConfig,
+    LoraConfig,
     AdaLoraConfig ,  # https://www.zhihu.com/question/596950521/answer/3109759716
     get_peft_model,
     set_peft_model_state_dict,
@@ -372,7 +372,7 @@ def train(global_args):
     # LoRA
     #target_modules = TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING['chatglm']
     target_modules = find_all_linear_names(model)
-    lora_config = AdaLoraConfig(   # LoraConfig
+    lora_config = LoraConfig(   # AdaLoraConfig 和 qlora好像有冲突
         r=global_args.lora_rank,
         lora_alpha=global_args.lora_alpha,
         target_modules=target_modules,
