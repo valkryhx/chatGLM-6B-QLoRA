f69f025d8688606a64b75caeae1645c3f6ee9598
valkryhx <283306449@qq.com>
2023-08-07 17:21:06 +0800

    Update rm_qlora_chatglm2.py

--- rm_qlora_chatglm2.py
+++ rm_qlora_chatglm2.py
@@ -359,16 +359,19 @@ class RewardTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         # print('inputs["input_ids_j"]: ', inputs["input_ids_j"].shape)
         # print('inputs["attention_mask_j"]: ', inputs["attention_mask_j"].shape)
-        rewards_j = model(
-            chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
+        #rewards_j = model(chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
         # print("rewards_j: ", type(rewards_j), rewards_j.shape)
 
         # print('inputs["input_ids_k"]: ', inputs["input_ids_k"].shape)
         # print('inputs["attention_mask_k"]: ', inputs["attention_mask_k"].shape)
-        rewards_k = model(
-            rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
+        #rewards_k = model(rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
         # print("rewards_k: ", type(rewards_k), rewards_k.shape)
-        
+
+        total_rewards_j_k =  model(
+                   chosen_input_ids=inputs["input_ids_j"]+inputs["input_ids_k"], 
+                   chosen_attention_mask=inputs["attention_mask_j"] + inputs["attention_mask_k"]
+        reward_j = total_rewards_j_k[:len(inputs["input_ids_j"])]["chosen_reward"]
+        reward_k = total_rewards_j_k[len(inputs["input_ids_j"]):]["reject_reward"]
         loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
         if return_outputs:
             return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
