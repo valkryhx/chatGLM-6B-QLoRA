e9f0b39a5a24b3283bb2ef4ef745bbd5ea997d9b
valkryhx <283306449@qq.com>
2023-08-09 02:50:20 +0800

    Update rm_new.py

--- rm_new.py
+++ rm_new.py
@@ -195,10 +195,9 @@ class RewardModel(PreTrainedModel):
                     end_ind = max(c_ind, r_ind)
                     divergence_ind = check_divergence[0]
                 assert divergence_ind > 0
-                c_truncated_reward = chosen_reward[divergence_ind:end_ind]
-                r_truncated_reward = rejected_reward[divergence_ind:end_ind]
-                chosen_mean_scores.append(
-                    chosen_reward[c_ind - 1])  #use the end score for reference
+                c_truncated_reward = chosen_reward.view(-1)[divergence_ind:end_ind]
+                r_truncated_reward = rejected_reward.view(-1)[divergence_ind:end_ind]
+                chosen_mean_scores.append(chosen_reward[c_ind - 1])  #use the end score for reference
                 rejected_mean_scores.append(rejected_reward[r_ind - 1])
 
                 loss += -torch.nn.functional.logsigmoid(c_truncated_reward -
