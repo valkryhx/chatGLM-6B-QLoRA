795b4ea8cbd418fcad4390245f1b7f4ad71dfbeb
valkryhx <283306449@qq.com>
2023-08-09 01:47:07 +0800

    Update rm_new.py

--- rm_new.py
+++ rm_new.py
@@ -340,20 +340,20 @@ class RewardTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         # print('inputs["input_ids_j"]: ', inputs["input_ids_j"].shape)
         # print('inputs["attention_mask_j"]: ', inputs["attention_mask_j"].shape)
-        rewards_j = model.forward(
-            chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
+        #rewards_j = model.forward(
+        #    chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
         # print("rewards_j: ", type(rewards_j), rewards_j.shape)
 
         # print('inputs["input_ids_k"]: ', inputs["input_ids_k"].shape)
         # print('inputs["attention_mask_k"]: ', inputs["attention_mask_k"].shape)
-        rewards_k = model.forward(
-            rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
+        #rewards_k = model.forward(
+        #    rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
         # print("rewards_k: ", type(rewards_k), rewards_k.shape)
-        
-        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        res = model.forward(input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"])
+        #loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
         if return_outputs:
-            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
-        return loss
+            return res["loss"], {"rewards_j": res["chosen_reward"], "rewards_k": res["reject_reward"]}
+        return res["loss"]
 
     def save_model(self, output_dir: Optional[str] = None, _internal_call: bool = False):
         """只保存adapter"""
