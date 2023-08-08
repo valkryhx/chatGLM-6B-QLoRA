c17b6f22e0a58b509b5cc5be9a41e0d3ef3aae9e
valkryhx <283306449@qq.com>
2023-08-08 13:54:33 +0800

    Update rm_qlora_chatglm2.py

--- rm_qlora_chatglm2.py
+++ rm_qlora_chatglm2.py
@@ -126,7 +126,7 @@ class PairWiseLoss(nn.Module):
 ## part of codes are from https://github.com/valkryhx/ChatGLM-LoRA-RLHF-PyTorch/blob/4d3b8df2d6b7908a924b91b339f4468ed357761e/reward_model.py#L54
 
 # 定义奖励模型 原理是在chatglm2模型的基础上加上v_head层 v_head层的shape为(hidden_size, 1) 也就是输出为一个float 值，注意dtype=torch.float32
-class RewardModel(PreTrainedModel):
+class RewardModel_old(PreTrainedModel):
     supports_gradient_checkpointing = True
 
     def __init__(self, config, model, tokenizer):
@@ -228,7 +228,8 @@ class RewardModel(PreTrainedModel):
         }
 
 
-def preprocess_function(example,tokenizer):
+            
+def preprocess_function_old(example,tokenizer):
     new_examples = {
         "input_ids_j": [],
         "attention_mask_j": [],
@@ -252,8 +253,189 @@ def preprocess_function(example,tokenizer):
         new_examples["input_ids_k"].append(tokenized_k["input_ids"])
         new_examples["attention_mask_k"].append(tokenized_k["attention_mask"])
         
-
     return new_examples
+
+## Note that the following code is modified from
+## https://github.com/CarperAI/trlx/blob/main/examples/summarize_rlhf/reward_model/reward_model.py
+class RewardModel(nn.Module):
+
+    def __init__(self, base_model, tokenizer, num_padding_at_beginning=0):
+        super().__init__()
+        self.config = base_model.config
+        self.num_padding_at_beginning = num_padding_at_beginning
+        if hasattr(self.config, "word_embed_proj_dim"):
+            # `OPT` models use word_embed_proj_dim as final output
+            # https://github.com/huggingface/transformers/blob/main/src/transformers/models/opt/modeling_opt.py#L497
+            self.v_head = nn.Linear(self.config.word_embed_proj_dim,
+                                    1,
+                                    bias=False)
+        else:
+            # `gpt-neo(x)` models use `hidden_size` attribute names instead of `n_embd``
+            self.config.n_embd = self.config.hidden_size if hasattr(
+                self.config, "hidden_size") else self.config.n_embd
+            self.v_head = nn.Linear(self.config.n_embd, 1, bias=False)
+        self.rwtranrsformer = base_model
+        self.PAD_ID = tokenizer.pad_token_id
+
+    def gradient_checkpointing_enable(self):
+        self.rwtranrsformer.gradient_checkpointing_enable()
+
+    def gradient_checkpointing_disable(self):
+        self.rwtranrsformer.gradient_checkpointing_disable()
+
+    def forward(self,
+                input_ids=None,
+                past_key_values=None,
+                attention_mask=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                use_cache=False):
+        loss = None
+
+        transformer_outputs = self.rwtranrsformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache)
+
+        hidden_states = transformer_outputs[0]
+        rewards = self.v_head(hidden_states).squeeze(-1)
+        chosen_mean_scores = []
+        rejected_mean_scores = []
+
+        # Split the inputs and rewards into two parts, chosen and rejected
+        assert len(input_ids.shape) == 2
+        bs = input_ids.shape[0] // 2
+        seq_len = input_ids.shape[1]
+
+        chosen_ids = input_ids[:bs]  # bs x seq x 1
+        rejected_ids = input_ids[bs:]
+        chosen_rewards = rewards[:bs]
+        rejected_rewards = rewards[bs:]
+
+        # Compute pairwise loss. Only backprop on the different tokens before padding
+        loss = 0
+        for i in range(bs):
+            chosen_id = chosen_ids[i]
+            rejected_id = rejected_ids[i]
+            chosen_reward = chosen_rewards[i]
+            rejected_reward = rejected_rewards[i]
+
+            c_inds = (chosen_id == self.PAD_ID).nonzero()
+            c_ind = c_inds[self.num_padding_at_beginning].item() if len(
+                c_inds
+            ) > self.num_padding_at_beginning else seq_len  # OPT model pads the first token, so we need to use the second padding token as the end of the sequence
+            check_divergence = (chosen_id != rejected_id).nonzero()
+
+            if len(check_divergence) == 0:
+                end_ind = rejected_reward.size(-1)
+                divergence_ind = end_ind - 1
+                r_ind = c_ind
+            else:
+                # Check if there is any padding otherwise take length of sequence
+                r_inds = (rejected_id == self.PAD_ID).nonzero()
+                r_ind = r_inds[self.num_padding_at_beginning].item(
+                ) if len(r_inds) > self.num_padding_at_beginning else seq_len
+                end_ind = max(c_ind, r_ind)
+                divergence_ind = check_divergence[0]
+            assert divergence_ind > 0
+            c_truncated_reward = chosen_reward[divergence_ind:end_ind]
+            r_truncated_reward = rejected_reward[divergence_ind:end_ind]
+            chosen_mean_scores.append(
+                chosen_reward[c_ind - 1])  #use the end score for reference
+            rejected_mean_scores.append(rejected_reward[r_ind - 1])
+
+            loss += -torch.nn.functional.logsigmoid(c_truncated_reward -
+                                                    r_truncated_reward).mean()
+
+        loss = loss / bs
+        chosen_mean_scores = torch.stack(chosen_mean_scores)
+        rejected_mean_scores = torch.stack(rejected_mean_scores)
+        # return {
+        #     "loss": loss,
+        #     "chosen_mean_scores": chosen_mean_scores,
+        #     "rejected_mean_scores": rejected_mean_scores,
+        # }
+        return loss
+
+    def forward_value(self,
+                      input_ids=None,
+                      attention_mask=None,
+                      past_key_values=None,
+                      position_ids=None,
+                      head_mask=None,
+                      inputs_embeds=None,
+                      return_value_only=False,
+                      prompt_length=0,
+                      use_cache=False):
+
+        transformer_outputs = self.rwtranrsformer(
+            input_ids,
+            past_key_values=past_key_values,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache)
+        hidden_states = transformer_outputs[0]
+        values = self.v_head(hidden_states).squeeze(-1)
+        if return_value_only:
+            return values
+        else:
+            # [0 0 0 0 prompt, answer, 0 0 0 0 ] for step 3, we have padding at the beginning
+            # [prompt, answer, 0, 0, 0, 0] this is normal
+            assert prompt_length > 1, "prompt_length must be greater than 1 to help select the end score"
+            bs = values.size(0)
+            seq_len = input_ids.shape[1]
+            chosen_end_scores = [
+            ]  # we use this name for consistency with the original forward function
+            for i in range(bs):
+                input_id = input_ids[i]
+                value = values[i]
+
+                c_inds = (input_id[prompt_length:] == self.PAD_ID).nonzero()
+                # here we only use the answer part of the sequence so we do not need to care about the padding at the beginning
+                c_ind = c_inds[0].item() + prompt_length if len(
+                    c_inds) > 0 else seq_len
+                chosen_end_scores.append(value[c_ind - 1])
+            return {
+                "values": values,
+                "chosen_end_scores": torch.stack(chosen_end_scores),
+            }
+
+def preprocess_function2(example,tokenizer):
+    #
+    # for question, response_j, response_k in zip(examples["question"], examples["response_j"], examples["response_k"]):
+    #for question, response_j, response_k in zip(example["user_input"], example["completion_a"], example["completion_b"]):
+    chatglm2_prompt = "[Round 1]\n\n问：{}\n\n答：{}\n\n"
+    tokenized_j = tokenizer(
+            #"Question: " + question + "\n\nAnswer: " + response_j, truncation=True
+              chatglm2_prompt.format(question,response_j),truncation=True
+              )
+    tokenized_k = tokenizer(
+            #"Question: " + question + "\n\nAnswer: " + response_k, truncation=True
+              chatglm2_prompt.format(question,response_k),truncation=True
+              )
+        
+        # new_examples["input_ids_j"].append(tokenized_j["input_ids"])
+        # new_examples["attention_mask_j"].append(tokenized_j["attention_mask"])
+        # new_examples["input_ids_k"].append(tokenized_k["input_ids"])
+        # new_examples["attention_mask_k"].append(tokenized_k["attention_mask"])
+    return [ tokenized_j["input_ids"]  ,tokenized_j["attention_mask"] , tokenized_k["input_ids"] ,tokenized_k["attention_mask"]]
+
+class DataCollatorReward:
+
+    def __call__(self, data):
+        batch = {}
+        batch["input_ids"] = torch.cat([f[0]
+                                        for f in data] + [f[2] for f in data],
+                                       dim=0)
+        batch["attention_mask"] = torch.cat([f[1] for f in data] +
+                                            [f[3] for f in data],
+                                            dim=0)
+        return batch
 
 def get_rm_datset(data_path, tokenizer, max_samples=-1,global_args=None):
     """读取本地包含json/jsonl文件的目录，将目录中所有文件作为dataset，只采样max_samples个参与训练/评估。
@@ -282,8 +464,8 @@ def get_rm_datset(data_path, tokenizer, max_samples=-1,global_args=None):
     logger.info("preprocessing dataset...")
     
     tokenized_dataset = data['train'].map(
-                                lambda example: preprocess_function(example, tokenizer=tokenizer),
-                                batched=True, 
+                                lambda example: preprocess_function2(example, tokenizer=tokenizer),
+                                batched=False, 
                                 remove_columns=data['train'].column_names)
     # 验证打印一些信息
     # print(f"tokenized_dataset={tokenized_dataset}")
@@ -298,7 +480,7 @@ def get_rm_datset(data_path, tokenizer, max_samples=-1,global_args=None):
     return tokenized_dataset
 
 # 不加datclass那就写__init__函数 参考https://github.com/valkryhx/chatGLM-6B-QLoRA/blob/main/sft_multi_turn_qlora_chatglm2.py#L325
-class RewardDataCollatorWithPadding:
+class RewardDataCollatorWithPadding2:
     tokenizer: PreTrainedTokenizerBase
     padding: Union[bool, str, PaddingStrategy] = True
     max_length: Optional[int] = None
@@ -357,23 +539,23 @@ class RewardDataCollatorWithPadding:
 
 class RewardTrainer(Trainer):
     # Define how to compute the reward loss. We use the InstructGPT pairwise logloss: https://arxiv.org/abs/2203.02155
-    def compute_loss(self, model, inputs, return_outputs=False):
-        # print('inputs["input_ids_j"]: ', inputs["input_ids_j"].shape)
-        # print('inputs["attention_mask_j"]: ', inputs["attention_mask_j"].shape)
-        rewards_j = model(
-            chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
-        # print("rewards_j: ", type(rewards_j), rewards_j.shape)
+    # def compute_loss(self, model, inputs, return_outputs=False):
+    #     # print('inputs["input_ids_j"]: ', inputs["input_ids_j"].shape)
+    #     # print('inputs["attention_mask_j"]: ', inputs["attention_mask_j"].shape)
+    #     rewards_j = model(
+    #         chosen_input_ids=inputs["input_ids_j"], chosen_attention_mask=inputs["attention_mask_j"])["chosen_reward"]
+    #     # print("rewards_j: ", type(rewards_j), rewards_j.shape)
 
-        # print('inputs["input_ids_k"]: ', inputs["input_ids_k"].shape)
-        # print('inputs["attention_mask_k"]: ', inputs["attention_mask_k"].shape)
-        rewards_k = model(
-            rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
-        # print("rewards_k: ", type(rewards_k), rewards_k.shape)
+    #     # print('inputs["input_ids_k"]: ', inputs["input_ids_k"].shape)
+    #     # print('inputs["attention_mask_k"]: ', inputs["attention_mask_k"].shape)
+    #     rewards_k = model(
+    #         rejected_input_ids=inputs["input_ids_k"], rejected_attention_mask=inputs["attention_mask_k"])["reject_reward"]
+    #     # print("rewards_k: ", type(rewards_k), rewards_k.shape)
         
-        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
-        if return_outputs:
-            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
-        return loss
+    #     loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+    #     if return_outputs:
+    #         return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+    #     return loss
 
     def save_model(self, output_dir: Optional[str] = None, _internal_call: bool = False):
         """只保存adapter"""
@@ -509,10 +691,10 @@ def train(global_args):
     eval_dataset = get_rm_datset(data_path=global_args.eval_data_path, tokenizer=tokenizer, max_samples=global_args.num_eval_samples,global_args=global_args)
     
     ### STEP 2 定义 data collator
-    rm_data_collator = RewardDataCollatorWithPadding(
-                                   tokenizer=tokenizer, 
-                                   max_length=global_args.max_length, 
-                                   pad_to_multiple_of=8)
+    # rm_data_collator = RewardDataCollatorWithPadding(
+    #                                tokenizer=tokenizer, 
+    #                                max_length=global_args.max_length, 
+    #                                pad_to_multiple_of=8)
 
   
      
@@ -524,12 +706,14 @@ def train(global_args):
                                   bnb_4bit_quant_type='nf4',
                                   bnb_4bit_use_double_quant=True,
                                   bnb_4bit_compute_dtype=_compute_dtype_map[global_args.compute_dtype])
-        
+    model_config = AutoConfig.from_pretrained(global_args.model_name_or_path)
+    model_config.dropout = 0.0  # RM dropout=0
     model = AutoModel.from_pretrained(global_args.model_name_or_path,
                                           trust_remote_code=True,                           
                                           load_in_4bit=global_args.load_in_4bit,
                                           torch_dtype=torch.float16,
                                           quantization_config=q_config,
+                                          config=model_config
                                            # empty_init这是最关键的参数 如果不设置 那即使用deepspeed也oom
                                   # 当您使用 AutoModel.from_pretrained() 方法加载预训练模型时，模型权重会被存储在 PyTorch 的 nn.Parameter 对象中。
                                   # 在没有指定 empty_init=False 参数时，nn.Parameter 对象的值将被初始化为全零的张量。
@@ -541,10 +725,12 @@ def train(global_args):
                                           empty_init=False,   # https://github.com/THUDM/ChatGLM-6B/issues/530
                                           #device_map=new_hf_device_map,
                                           # device_map="auto"   # add 20230713
+                                      
                                      )
 
     
-    
+    model = RewardModel(model,tokenizer)
+    print(model)
     print(f'memory footprint of model: {model.get_memory_footprint()/(1024*1024*1024)} GB')
     # 
     # .gradient_checkpointing_enable()
@@ -593,9 +779,8 @@ def train(global_args):
         else:
             logger.info(f'Checkpoint {checkpoint_name} not found')
 
-    model.print_trainable_parameters()
-    model = RewardModel(model.config, model.transformer, tokenizer)
-    print(model)
+    #model.print_trainable_parameters()
+    
     #model = DistributedDataParallel(model.cuda()) 
     #model._set_static_graph()
     logger.info(f"Finished loading model and tokenizer")
@@ -624,7 +809,7 @@ def train(global_args):
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
     compute_metrics=compute_metrics,
-    data_collator=rm_data_collator,
+    data_collator=DataCollatorReward(),
        )
 
     #model.config.use_cache = False
