ca42de3111b857eeae88a6286f616b29575b3138
valkryhx <283306449@qq.com>
2023-08-08 15:55:03 +0800

    Update rm_qlora_chatglm2.py

--- rm_qlora_chatglm2.py
+++ rm_qlora_chatglm2.py
@@ -546,6 +546,10 @@ class DataCollatorReward_new:
     ## data = list of [{"input_ids_j":XX,"attention_mask_j":XX,"input_ids_k":XX,"attention_mask_k":XX}]
     def __call__(self, data):
         batch = {}
+        print(f"type_data[0]['input_ids_j'] = {type(data[0]['input_ids_j'])}")
+        print(f"type_data[0]["input_ids_k"] = {type(data[0][input_ids_k'])}")
+        print(f"type_data[0]["attention_mask_j"] = {type(data[0][attention_mask_j'])}")
+        print(f"type_data[0]["attention_mask_k"] = {type(data[0][attention_mask_k'])}")
         batch["input_ids"] = torch.cat([f["input_ids_j"] for f in data] + [f["input_ids_k"] for f in data],
                                         dim=0)
         batch["attention_mask"] = torch.cat([f["attention_mask_j"] for f in data] + [f["attention_mask_k"] for f in data],
