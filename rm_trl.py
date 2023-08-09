408a6db1cfd389cff3410dfc9836e5dfb33b84d4
valkryhx <283306449@qq.com>
2023-08-09 19:13:52 +0800

    Update rm_trl.py

--- rm_trl.py
+++ rm_trl.py
@@ -323,8 +323,8 @@ class RewardModel(PreTrainedModel):
             print(f"total_reward.shape={total_reward.shape}")
             batch_size = input_ids.size(0) // 2
             print(f"batch_size={batch_size}")
-            print(f"total_reward[-1]={total_reward[-1]},{total_reward[-1].shape}")
-            chosen_reward, reject_reward = total_reward[-1].split(batch_size, dim=0)
+            # 注意是 total_reward[:,-1] 而不是  total_reward[-1] 前面保留了维度dim=0和dim=1最后一列 后者是dim=1全保留但是dim=0最后一行
+            chosen_reward, reject_reward = total_reward[:,-1].split(batch_size, dim=0)
             loss = -torch.log(torch.sigmoid(chosen_reward - reject_reward)).mean()
             #logger.error(f"use new method2,loss ={loss}")
 
