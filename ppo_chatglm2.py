ab40435025671cc4c72a1d48bb79bccdb4e65e78
valkryhx <283306449@qq.com>
2023-08-14 13:33:36 +0800

    Update ppo_chatglm2.py

--- ppo_chatglm2.py
+++ ppo_chatglm2.py
@@ -469,9 +469,9 @@ def get_rewards(
     _, _, values = reward_model(**batch, output_hidden_states=True, return_dict=True)
     #rewards = [reward for reward in values[:, -1].float().detach().cpu()] # use fp32 type
     #rewards = values[-1]  # https://github.com/valkryhx/chatGLM-6B-QLoRA/blob/main/rm_3.py#L820C30-L820C40
-    rewards= values[:,-1].view(-1).tolist()
-    logger.error(f"rewards in get_rewards={rewards}")
-    return rewards
+    _rewards= values[:,-1].view(-1).tolist()
+    logger.error(f"_rewards in get_rewards={_rewards}")
+    return _rewards
 
 # We then define the arguments to pass to the `generate` function. These arguments
 # are passed to the `generate` function of the PPOTrainer, which is a wrapper around
