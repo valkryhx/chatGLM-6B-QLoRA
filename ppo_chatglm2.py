b6b8df3858b79b288d039dfb3027e2d075a72ffc
valkryhx <283306449@qq.com>
2023-08-14 21:51:08 +0800

    Update ppo_chatglm2.py

--- ppo_chatglm2.py
+++ ppo_chatglm2.py
@@ -469,7 +469,7 @@ def get_rewards(
     _, _, values = reward_model(**batch, output_hidden_states=True, return_dict=True)
     #rewards = [reward for reward in values[-1].float().detach().cpu()] # use fp32 type
     #rewards = [reward for reward in values[-1].to(torch.float32)] # 也可以这么写
-    rewards = [reward for reward in values.to(torch.float32)] # 也可以这么写
+    rewards = [reward for reward in values.to(torch.float32)].permute(1,0) # 也可以这么写
     #rewards = values[-1]  # https://github.com/valkryhx/chatGLM-6B-QLoRA/blob/main/rm_3.py#L820C30-L820C40
     #rewards= values[:,-1].view(-1).tolist()
     #_rewards = [reward for reward in values[-1].to(torch.float32)] # use float32 type 这个很有前途
