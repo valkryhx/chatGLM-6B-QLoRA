7fce02a57ef6dc2f3d6ac05190d1566f5316088e
valkryhx <283306449@qq.com>
2023-08-23 23:03:36 +0800

    Update dpo_qlora_chatglm2.py

--- dpo_qlora_chatglm2.py
+++ dpo_qlora_chatglm2.py
@@ -64,7 +64,8 @@ class ScriptArguments:
 
     max_prompt_length: Optional[int] = field(default=512, metadata={"help": "the maximum prompt length"})
     max_length: Optional[int] = field(default=1024, metadata={"help": "the maximum sequence length"})
-    max_steps: Optional[int] = field(default=1000, metadata={"help": "max number of training steps"})
+    max_steps: Optional[int] = field(default=-1, metadata={"help": "max number of training steps and overrides num_train_epochs: if set positive"})
+    num_train_epochs: Optional[float] = field(default=3, metadata={"help": "max number of training epochs"})
     logging_steps: Optional[int] = field(default=10, metadata={"help": "the logging frequency"})
     save_steps: Optional[int] = field(default=100, metadata={"help": "the saving frequency"})
     eval_steps: Optional[int] = field(default=100, metadata={"help": "the evaluation frequency"})
