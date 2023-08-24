cc990ff0517fda822694018bcde7cf1c1f2c6371
valkryhx <283306449@qq.com>
2023-08-24 14:22:57 +0800

    Update dpo_qlora_chatglm2.py

--- dpo_qlora_chatglm2.py
+++ dpo_qlora_chatglm2.py
@@ -129,7 +129,7 @@ def get_stack_exchange_paired(
     original_columns = dataset.column_names
 
     if sanity_check:
-        dataset = dataset.select(range(min(len(dataset), 100)))
+        dataset = dataset.select(range(min(len(dataset), 200)))
 
     def return_prompt_and_responses(samples) -> Dict[str, str]:
         return {
