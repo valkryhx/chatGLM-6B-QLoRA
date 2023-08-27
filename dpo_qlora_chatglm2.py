9d144c6133ded0eb58cb60dfee40fe1fd941645a
valkryhx <283306449@qq.com>
2023-08-27 17:00:23 +0800

    Update dpo_qlora_chatglm2.py

--- dpo_qlora_chatglm2.py
+++ dpo_qlora_chatglm2.py
@@ -238,6 +238,75 @@ def torch_gc() -> None:
         torch.cuda.empty_cache()
         torch.cuda.ipc_collect()
 
+
+def create_reference_model(
+    model: PreTrainedModelWrapper, num_shared_layers: int = None, pattern: str = None
+) -> PreTrainedModelWrapper:
+    """
+    Creates a static reference copy of a model. Note that model will be in `.eval()` mode.
+
+    Args:
+        model (`PreTrainedModelWrapper`): The model to be copied.
+        num_shared_layers (`int`, *optional*): The number of initial layers that are shared between both models and kept frozen.
+        pattern (`str`, *optional*): The shared layers are selected with a string pattern
+            (e.g. "transformer.h.{layer}" for GPT2) and if a custom pattern is necessary it can be passed here.
+
+    Returns
+        `PreTrainedModelWrapper`
+    """
+
+    parameter_names = [n for n, _ in model.named_parameters()]
+    ref_model = deepcopy(model)
+
+    # if no layers are shared, return copy of model
+    if num_shared_layers is None:
+        for param_name in parameter_names:
+            param = ref_model.get_parameter(param_name)
+            param.requires_grad = False
+        return ref_model.eval()
+
+    # identify layer name pattern
+    if pattern is not None:
+        pattern = pattern.format(layer=num_shared_layers)
+    else:
+        for pattern_candidate in LAYER_PATTERNS:
+            pattern_candidate = pattern_candidate.format(layer=num_shared_layers)
+            if any([pattern_candidate in name for name in parameter_names]):
+                pattern = pattern_candidate
+                break
+
+    if pattern is None:
+        raise ValueError("Layer pattern could not be matched.")
+
+    # divide parameters in shared and unshared parameter lists
+    shared_param_list = []
+    unshared_param_list = []
+
+    shared_parameter = True
+    for name, param in model.named_parameters():
+        if pattern in name:
+            shared_parameter = False
+        if shared_parameter:
+            shared_param_list.append(name)
+        else:
+            unshared_param_list.append(name)
+
+    # create reference of the original parameter if they are shared
+    for param_name in shared_param_list:
+        param = model.get_parameter(param_name)
+        param.requires_grad = False
+
+        ref_param = ref_model.get_parameter(param_name)  # noqa
+        ref_param = param  # noqa
+
+    # for all other parameters just make sure they don't use gradients
+    for param_name in unshared_param_list:
+        param = ref_model.get_parameter(param_name)
+        param.requires_grad = False
+
+    return ref_model.eval()
+
+
 class MyDPOTrainer(DPOTrainer): 
 
     def __init__(
@@ -286,7 +355,7 @@ class MyDPOTrainer(DPOTrainer):
             # The `model` with adapters turned off will be used as the reference model
             self.ref_model = None
         else:
-            self.ref_model = create_reference_model(model)
+            self.ref_model = create_reference_model(model).to("cuda:1")
 
         if data_collator is None:
             if tokenizer is None:
@@ -585,8 +654,8 @@ if __name__ == "__main__":
     logger.info("prepare my dpo_trainer")
     my_dpo_trainer = MyDPOTrainer(
         model,
-        data_collator = my_data_collator ,
-        ref_model =model_ref,#None, #model_ref,
+        #data_collator = my_data_collator ,
+        ref_model =None, #model_ref,
         args=training_args,
         beta=script_args.beta,
         train_dataset=train_dataset,
