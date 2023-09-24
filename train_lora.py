9ffc22051beb2ddb4252120cbad8dc8b77d7a5c7
valkryhx <283306449@qq.com>
2023-09-24 13:07:53 +0800

    Update train_lora.py

--- train_lora.py
+++ train_lora.py
@@ -127,13 +127,13 @@ def tokenize_func(example, tokenizer, global_args, ignore_label_id=-100):
     q_ids = tokenizer.encode(text=question, add_special_tokens=False)
     a_ids = tokenizer.encode(text=answer, add_special_tokens=False)
     #这里不使用 tokenizer.build_inputs_with_special_tokens(q_ids, a_ids)，因为这个方法可能其他模型没有
-    total_ids = tokenizer.encode(text=question+answer ,add_special_tokens=False)[:global_args.max_length]
+    input_ids = tokenizer.encode(text= question + answer ,add_special_tokens=False)[:global_args.max_length]
     #可能需要加上下面的
-    #total_ids = total_ids + [2] # [2]是eos token id 确保最后是结束符eos 不然训练后可能模型会出现一直输出的现象。
-    question_length = len(q_ids)
-    labels = [ignore_label_id] * question_length + total_ids[question_length:]
-    assert len(total_ids)  == len(labels) 
-    return {'input_ids': total_ids, 'labels': labels}
+    #input_ids = input_ids + [2] # [2]是eos token id 确保最后是结束符eos 不然训练后可能模型会出现一直输出的现象。
+    q_length = len(q_ids)
+    labels = [ignore_label_id] * q_length + input_ids[q_length:]
+    assert len(input_ids)  == len(labels) 
+    return {'input_ids': input_ids, 'labels': labels}
 
 #20230923 这个不再使用
 def tokenize_func_old_not_use(example, tokenizer, global_args, ignore_label_id=-100):
